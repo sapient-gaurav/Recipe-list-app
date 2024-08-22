479bc9b93f9300d519308b09416eaa7274b2ac60
sapientcodelabs-rushal <rushal.vaghani@sapientcodelabs.xyz>
2024-08-22 11:18:18 +0530

    Updates

--- src/store/registration/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReducer from './UserSlice'
-import AdditemReducer from "./AdditemSlice"
-import ActiveReducer from "./ActiveSlice"
-
-export const store = configureStore({
-  reducer: { UserReducer,AdditemReducer,ActiveReducer },
-});
